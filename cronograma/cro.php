1ee1b2aba97f99706a5d68649b603f8093818e93
gbllucas <49254851+gbllucas@users.noreply.github.com>
2019-04-28 19:19:07 -0300

    projeto t.i responsável protótipo cronograma
    horários de aula

---
+++ cronograma/cro.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+	<link rel="stylesheet" href="estilos.css">
+	<title>Tabela</title>
+</head>
+<body>
+	<table class="cro">
+		<caption>Cronograma</caption>
+		<thead>
+			<th>horário</th>
+			<th>segunda</th>
+			<th>Terça</th>
+			<th>Quarta</th>
+			<th>Quinta</th>
+			<th>Sexta</th>
+		</thead>
+
+		<tbody>
+			<tr>
+				<td><input type="time" class ="hora"></td>
+				<td><input type="text" class =" modalidade"name="modalidade" autofocus maxlength="20"></td>
+				<td><input type="text" class =" modalidade"name="modalidade" autofocus maxlength="20"></td>
+				<td><input type="text" class =" modalidade"name="modalidade" autofocus maxlength="20"></td>
+				<td><input type="text" class =" modalidade"name="modalidade" autofocus maxlength="20"></td>
+				<td><input type="text" class =" modalidade"name="modalidade" autofocus maxlength="20"></td>
+			</tr>
+				<td><input type="time" class ="hora"></td>
+				<td><input type="text" class =" modalidade"name="modalidade" autofocus maxlength="20"></td>
+				<td><input type="text" class =" modalidade"name="modalidade" autofocus maxlength="20"></td>
+				<td><input type="text" class =" modalidade"name="modalidade" autofocus maxlength="20"></td>
+				<td><input type="text" class =" modalidade"name="modalidade" autofocus maxlength="20"></td>
+				<td><input type="text" class =" modalidade"name="modalidade" autofocus maxlength="20"></td>
+			<tr>
+				<td><input type="time" class ="hora"></td>
+				<td><input type="text" class =" modalidade"name="modalidade" autofocus maxlength="20"></td>
+				<td><input type="text" class =" modalidade"name="modalidade" autofocus maxlength="20"></td>
+				<td><input type="text" class =" modalidade"name="modalidade" autofocus maxlength="20"></td>
+				<td><input type="text" class =" modalidade"name="modalidade" autofocus maxlength="20"></td>
+				<td><input type="text" class =" modalidade"name="modalidade" autofocus maxlength="20"></td>
+			</tr>
+
+			<tr>
+				<td><input type="time" class ="hora"></td>
+				<td><input type="text" class =" modalidade"name="modalidade" autofocus maxlength="20"></td>
+				<td><input type="text" class =" modalidade"name="modalidade" autofocus maxlength="20"></td>
+				<td><input type="text" class =" modalidade"name="modalidade" autofocus maxlength="20"></td>
+				<td><input type="text" class =" modalidade"name="modalidade" autofocus maxlength="20"></td>
+				<td><input type="text" class =" modalidade"name="modalidade" autofocus maxlength="20"></td>
+			</tr>
+				<td><input type="time" class ="hora"></td>
+				<td><input type="text" class =" modalidade"name="modalidade" autofocus maxlength="20"></td>
+				<td><input type="text" class =" modalidade"name="modalidade" autofocus maxlength="20"></td>
+				<td><input type="text" class =" modalidade"name="modalidade" autofocus maxlength="20"></td>
+				<td><input type="text" class =" modalidade"name="modalidade" autofocus maxlength="20"></td>
+				<td><input type="text" class =" modalidade"name="modalidade" autofocus maxlength="20"></td>
+		</tbody>
+	</table>
+</body>
+<input type="submit">
+</html>
